96dea6954089d0c5a7382d0c186ff7b03191976f
Nikesh Kumar <63540536+ItsSlade@users.noreply.github.com>
2024-04-02 15:33:55 +0530

    Create MinimumAbsoluteDifferenceInBST.java

---
+++ problem-of-the-day/MinimumAbsoluteDifferenceInBST.java
@@ -0,0 +1,14 @@
+/*The Node structure is defined as
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+};
+*/
+
+class Solution {
+    int absolute_diff(Node root) {
+        //Your code here
+    }
+}
